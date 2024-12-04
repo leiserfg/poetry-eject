beef363b1027cf34a175da67946f91ffefc96665
leiserfg <leiserfg@gmail.com>
2024-12-04 11:55:50 +0100

    Add devenv

---
+++ devenv.nix
@@ -0,0 +1,61 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  # https://devenv.sh/basics/
+
+  # https://devenv.sh/packages/
+  packages = [pkgs.git];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    package = pkgs.python312;
+    poetry.enable = true;
+    poetry.activate.enable = true;
+    poetry.install.enable = true;
+  };
+
+  languages.terraform.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  git-hooks.hooks = {
+    ruff.enable = true;
+    ruff.excludes = ["settings"];
+    ruff-format.enable = true;
+    terraform-format.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
